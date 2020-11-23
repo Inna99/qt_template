5a8033eb0e47359c733659c2b25aee2678c50608
Rudolf Nemov <antibagr@yandex.ru>
2020-11-23 09:16:06 +0300

    Improve ui imports in app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from PyQt5 import QtWidgets, uic
 
-from config import UI_MAIN_WINDOW
+from config import UI_MAIN_WINDOW, DESIGN_DIR
 
-Ui_MainWindow, _ = uic.loadUiType(UI_MAIN_WINDOW)
+# Read more at
+# https://doc.bccnsoft.com/docs/PyQt5/designer.html#using-the-generated-code
+Ui_MainWindow, _ = uic.loadUiType(UI_MAIN_WINDOW, import_from=DESIGN_DIR)
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
