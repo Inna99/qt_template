451a2e9e3b7120bc73cab2c861f78a1ec6fbfa1a
Rudolf Nemov <antibagr@yandex.ru>
2020-11-25 17:20:00 +0300

    Refactor abstract code. Add system type checking in run.py

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,9 +1,7 @@
-import weakref
-
 import pytest
-
-from pytestqt import qt_compat
 from pytestqt.qt_compat import qt_api
+
+from qt.app import MainWindow
 
 
 def test_basics(qtbot):
@@ -11,11 +9,22 @@ def test_basics(qtbot):
     Basic test that works more like a sanity check to ensure we are setting up a QApplication
     properly and are able to display a simple event_recorder.
     """
+
     assert qt_api.QApplication.instance() is not None
     widget = qt_api.QWidget()
     qtbot.addWidget(widget)
     widget.setWindowTitle("W1")
     widget.show()
-
     assert widget.isVisible()
     assert widget.windowTitle() == "W1"
+
+def test_main_window(qtbot):
+    """
+    Same as above but testing the main window of template package
+    """
+
+    assert qt_api.QApplication.instance() is not None
+    mw = MainWindow()
+    qtbot.addWidget(mw)
+    mw.show()
+    assert mw.isVisible()
